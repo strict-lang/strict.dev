77424274769eaf0f76a99968e2412da4cbf7419c
Merlin Osayimwen <merlinosayimwen@gmail.com>
2020-02-11 22:12:00 +0100

    Remove outdated documents

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -1,22 +1,9 @@
-/**
- * Copyright (c) 2017-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
-// See https://docusaurus.io/docs/site-config
 const siteConfig = {
-  title: "Strict Programming Language", // Title for your website.
-  tagline:
-    "Strict is a simple to understand programming language for humans and AI",
+  title: "Strict Programming Language", 
+  tagline: "Write good code (fast)",
   cname: "strict.dev",
-  url: "https://strict.dev", // Your website URL
-  baseUrl: "/", // Base URL for your project */
-  // For github.io type URLs, you would set the url and baseUrl like:
-  //   url: 'https://facebook.github.io',
-  //   baseUrl: '/test-site/',
-  // Used for publishing and more
+  url: "https://strict.dev",
+  baseUrl: "/",
   projectName: "strict.dev",
   organizationName: "strict-lang",
   algolia: {
@@ -26,53 +13,28 @@ const siteConfig = {
   },
   headerLinks: [
     { search: true },
-    { doc: "Overview", label: "Docs" },
-    { doc: "Keywords", label: "API" },
+    { doc: "overview", label: "Docs" },
     { blog: true, label: "Blog" }
   ],
   headerIcon: "img/StrictLogoWhite64x64.png",
   footerIcon: "img/StrictLogoBlue128x128.png",
   favicon: "img/favicon.ico",
-
-  /* Colors for website */
   colors: {
     primaryColor: "#52A0C6",
     secondaryColor: "#21434C"
   },
-
-  /* Custom fonts for website */
   fonts: {
     myFont: ["Verdana", "Serif"]
   },
-
-  // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: `Copyright © ${new Date().getFullYear()} strict-lang`,
-
+  copyright: `Copyright © ${new Date().getFullYear()} The Strict contributors`,
   highlight: {
-    // Highlight.js theme to use for syntax highlighting in code blocks.
-    theme: "default"
+    theme: "androidstudio"
   },
-
-  // Add custom scripts here that would be placed in <script> tags.
   scripts: ["https://buttons.github.io/buttons.js"],
-
-  // On page navigation for the current documentation page.
   onPageNav: "separate",
-  // No .html extensions for paths.
   cleanUrl: true,
-
-  // For sites with a sizable amount of content, set collapsible to true.
-  // Expand/collapse the links and subcategories under categories.
-  //docsSideNavCollapsible: true,
-
-  // Show documentation's last contributor's name.
   enableUpdateBy: true,
-
-  // Show documentation's last update time.
   enableUpdateTime: true,
-
-  // You may provide arbitrary config keys to be used as needed by your
-  // template. For example, if you need your repo's URL...
   repoUrl: "https://github.com/strict-lang/strict.dev"
 };
 
